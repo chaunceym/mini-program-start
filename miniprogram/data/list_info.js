f03437d82705928fe591726d76c4d92301573d20
mxylovezh <18322276773@163.com>
2020-06-03 06:21:49 +0800

    mini
    完成list页面的列表渲染

---
+++ miniprogram/data/list_info.js
@@ -0,0 +1,85 @@
+const listData = [
+  {
+    date: '2020-06-03',
+    avatarUrl: '../static/images/avator1.png',
+    article_title: '孟祥宇',
+    article_image: '../static/images/blue.png',
+    article_content: '梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思',
+    good_data: '33',
+    start_data: '22'
+  },
+  {
+    date: '2020-06-03',
+    avatarUrl: '../static/images/avator1.png',
+    article_title: '孟祥宇',
+    article_image: '../static/images/blue2.png',
+    article_content: '梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思',
+    good_data: '33',
+    start_data: '22'
+  },
+  {
+    date: '2020-06-03',
+    avatarUrl: '../static/images/avator1.png',
+    article_title: '孟祥宇',
+    article_image: '../static/images/coffee.png',
+    article_content: '梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思',
+    good_data: '33',
+    start_data: '22'
+  },
+  {
+    date: '2020-06-03',
+    avatarUrl: '../static/images/avator1.png',
+    article_title: '孟祥宇',
+    article_image: '../static/images/coffee.png',
+    article_content: '梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思',
+    good_data: '33',
+    start_data: '22'
+  },
+  {
+    date: '2020-06-03',
+    avatarUrl: '../static/images/avator1.png',
+    article_title: '孟祥宇',
+    article_image: '../static/images/coffee.png',
+    article_content: '梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思',
+    good_data: '33',
+    start_data: '22'
+  },
+  {
+    date: '2020-06-03',
+    avatarUrl: '../static/images/avator1.png',
+    article_title: '孟祥宇',
+    article_image: '../static/images/coffee.png',
+    article_content: '梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思',
+    good_data: '33',
+    start_data: '22'
+  },
+  {
+    date: '2020-06-03',
+    avatarUrl: '../static/images/avator1.png',
+    article_title: '孟祥宇',
+    article_image: '../static/images/coffee.png',
+    article_content: '梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思',
+    good_data: '33',
+    start_data: '22'
+  },
+  {
+    date: '2020-06-03',
+    avatarUrl: '../static/images/avator1.png',
+    article_title: '孟祥',
+    article_image: '../static/images/coffee.png',
+    article_content: '梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思',
+    good_data: '33',
+    start_data: '22'
+  },
+  {
+    date: '2020-06-03',
+    avatarUrl: '../static/images/avator1.png',
+    article_title: '孟祥宇',
+    article_image: '../static/images/coffee.png',
+    article_content: '梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思想梦思',
+    good_data: '33',
+    start_data: '22'
+  }
+]
+
+module.exports = {listData}
