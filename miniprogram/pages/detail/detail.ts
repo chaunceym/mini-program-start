164813165e8efb146af0d1d17026701f753b50aa
mxylovezh <18322276773@163.com>
2020-06-03 10:44:34 +0800

    完成了收藏功能的数据持久化

--- miniprogram/pages/detail/detail.ts
+++ miniprogram/pages/detail/detail.ts
@@ -1,23 +1,73 @@
 import { decodedTextSpanIntersectsWith } from "../../../$node_modules/typescript/lib/tsserverlibrary";
 
 // miniprogram/pages/detail/detail.js
-const {listData} = require('../../data/list_info.js');
+const { listData } = require('../../data/list_info.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    detailDataInfo: {}
+    detailDataInfo: {},
+    isAddMyFavourite: false,
+    index: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options:any) {
-    const index = options.index
+  addMyFavourite() {
+    if (!this.data.isAddMyFavourite) {
+      this.setData({
+        isAddMyFavourite: true
+      })
+      wx.showToast({
+        title: '收藏成功',
+        icon: 'success',
+        duration: 1000
+      })
+
+    } else {
+      this.setData({
+        isAddMyFavourite: false
+      })
+      wx.showToast({
+        title: '取消收藏',
+        icon: 'success',
+        duration: 1000
+      })
+    }
+    wx.getStorage({
+      key: 'isCollected',
+      complete: (datas: any) => {
+        let collectionData = datas.data || {}
+        collectionData[this.data.index] = this.data.isAddMyFavourite
+        wx.setStorage({
+          key: 'isCollected',
+          data: collectionData
+        })
+      }
+    })
+  },
+
+  onLoad: function (options: any) {
+
     this.setData({
-      detailDataInfo: listData[index]
+      index: options.index
+    })
+    this.setData({
+      detailDataInfo: listData[this.data.index]
+    })
+    wx.getStorage({
+      key: 'isCollected',
+      complete: (datas:any) => {
+          const isCollected = datas.data
+          if(isCollected[options.index]){
+            this.setData({
+              isAddMyFavourite: true
+            })
+          }
+      }
     })
   },
 
